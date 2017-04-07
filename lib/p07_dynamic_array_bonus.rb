8e69f9f281e4e906d822f790fc670072167906b8
Brady Kimball <kimball.brady@gmail.com>
2017-04-07 15:32:00 -0700

    Implement bonus phase

--- lib/p07_dynamic_array_bonus.rb
+++ lib/p07_dynamic_array_bonus.rb
@@ -1,4 +1,7 @@
+require 'byebug'
+
 class StaticArray
+
   def initialize(capacity)
     @store = Array.new(capacity)
   end
@@ -25,6 +28,8 @@ class StaticArray
 end
 
 class DynamicArray
+  include Enumerable
+
   attr_reader :count
 
   def initialize(capacity = 8)
@@ -32,10 +37,39 @@ class DynamicArray
     @count = 0
   end
 
+  def last_index
+    i = capacity - 1
+    while i >= 0 && self[i].nil?
+      i -= 1
+    end
+    i < 0 ? nil : i
+  end
+
   def [](i)
+    if i < 0
+      idx = last_index + 1 + i
+      idx < 0 ? nil : @store[idx]
+    else
+      @store[i]
+    end
+  rescue
+    nil
   end
 
   def []=(i, val)
+    if self[i].nil?
+      @count += 1
+    end
+    until i < capacity
+      resize!
+    end
+
+    if i < 0
+      idx = last_index + 1 + i
+      self[idx] = val
+    else
+      @store[i] = val
+    end
   end
 
   def capacity
@@ -43,27 +77,78 @@ class DynamicArray
   end
 
   def include?(val)
+    i = 0
+    until self[i].nil?
+      return true if self[i] == val
+      i += 1
+    end
+    false
   end
 
   def push(val)
+    i = 0
+    until self[i] == nil
+      i += 1
+    end
+    self[i] = val
   end
 
   def unshift(val)
+    old_store = @store
+    @count = 0
+    @store = StaticArray.new(capacity + 1)
+    self[0] = val
+    i = 0
+    until old_store[i] == nil
+      self[i + 1] = old_store[i]
+      i += 1
+    end
   end
 
   def pop
+    i = 0
+    until self[i + 1].nil?
+      i += 1
+    end
+    @count -= 1
+    popped = self[i]
+    self[i] = nil
+    popped
   end
 
   def shift
+    old_store = @store
+    last_i = last_index
+    return nil if last_i.nil?
+    @count = 0
+    @store = StaticArray.new(capacity)
+    i = 1
+    until i == last_i + 1
+      self[i - 1] = old_store[i]
+      i += 1
+    end
+    old_store[0]
   end
 
   def first
+    self[0]
   end
 
   def last
+    i = 0
+    until self[i + 1] == nil
+      i += 1
+    end
+    self[i]
   end
 
   def each
+    i = 0
+    until i == capacity
+      yield(@store[i])
+      i += 1
+    end
+    self
   end
 
   def to_s
@@ -72,7 +157,12 @@ class DynamicArray
 
   def ==(other)
     return false unless [Array, DynamicArray].include?(other.class)
-    # ...
+    i = 0
+    until i == last_index + 1
+      return false unless self[i] == other[i]
+      i += 1
+    end
+    true
   end
 
   alias_method :<<, :push
@@ -81,5 +171,16 @@ class DynamicArray
   private
 
   def resize!
+    @count = 0
+    new_store = StaticArray.new(capacity * 2)
+    self.each_with_index do |num, i|
+      new_store[i] = num
+    end
+    @store = new_store
   end
+end
+
+if __FILE__ == $PROGRAM_NAME
+  a = DynamicArray.new
+  a[15] = 7
 end
