4d8f43b3bb4cd5f18cd5cd35ca653505a8ba5072
Brady Kimball <kimball.brady@gmail.com>
2017-04-07 11:06:14 -0700

    Initial Commit; Implement Phase 1: Int Set

---
+++ lib/p01_int_set.rb
@@ -0,0 +1,106 @@
+class MaxIntSet
+  attr
+
+  def initialize(max)
+    @store = Array.new(max, false)
+  end
+
+  def insert(num)
+    validate!(num)
+    return false if @store[num]
+    @store[num] = true
+  end
+
+  def remove(num)
+    validate!(num)
+    return nil unless include?(num)
+    @store[num] = false if include?(num)
+    num
+  end
+
+  def include?(num)
+    @store[num]
+  end
+
+  private
+
+  def is_valid?(num)
+    num.between?(0, @store.length - 1)
+  end
+
+  def validate!(num)
+    raise "Out of bounds" unless is_valid?(num)
+  end
+end
+
+
+class IntSet
+  def initialize(num_buckets = 20)
+    @store = Array.new(num_buckets) { Array.new }
+  end
+
+  def insert(num)
+    self[num] << num unless include?(num)
+  end
+
+  def remove(num)
+    self[num].delete(num) if include?(num)
+  end
+
+  def include?(num)
+    self[num].include?(num)
+  end
+
+  private
+
+  def [](num)
+    # optional but useful; return the bucket corresponding to `num`
+    @store[num % num_buckets]
+  end
+
+  def num_buckets
+    @store.length
+  end
+end
+
+class ResizingIntSet
+  attr_reader :count
+
+  def initialize(num_buckets = 20)
+    @store = Array.new(num_buckets) { Array.new }
+    @count = 0
+  end
+
+  def insert(num)
+    resize! if @count + 1 > num_buckets
+    self[num] << num unless include?(num)
+    @count += 1
+  end
+
+  def remove(num)
+    self[num].delete(num) if include?(num)
+    @count -= 1
+  end
+
+  def include?(num)
+    self[num].include?(num)
+  end
+
+  private
+
+  def [](num)
+    # optional but useful; return the bucket corresponding to `num`
+    @store[num % num_buckets]
+  end
+
+  def num_buckets
+    @store.length
+  end
+
+  def resize!
+    nums_to_move = @store.flatten
+    @count = 0
+    @store += Array.new(num_buckets) { Array.new }
+    nums_to_move.each { |num| insert(num) }
+  end
+end
